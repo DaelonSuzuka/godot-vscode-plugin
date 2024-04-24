33a33331424bcf3e8bd1a51c0906eebb0569eb51
David Kincaid <daelonsuzuka@gmail.com>
2024-04-24 19:19:18 -0400

    Add lezer

--- src/formatter/formatter.ts
+++ src/formatter/formatter.ts
@@ -1,19 +1,34 @@
 import * as vscode from "vscode";
 import { format_document } from "./textmate";
 import { createLogger } from "../utils";
+import { Tree } from "@lezer/common";
 
 const log = createLogger("formatter");
 
 export class FormattingProvider implements vscode.DocumentFormattingEditProvider {
-	constructor(private context: vscode.ExtensionContext) {
-		const selector = { language: "gdscript", scheme: "file" };
+	public parser;
 
-		context.subscriptions.push(
-			vscode.languages.registerDocumentFormattingEditProvider(selector, this),
-		);
+	constructor(private context: vscode.ExtensionContext) {
+		import("@gdquest/lezer-gdscript").then((module) => {
+			this.parser = module.parser;
+		});
+		
+		const selector = { language: "gdscript", scheme: "file" };
+		context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(selector, this));
 	}
 
 	public provideDocumentFormattingEdits(document: vscode.TextDocument) {
+		log.info("Formatting document");
+		const text = document.getText();
+		const tree: Tree = this.parser.parse(text);
+		const cursor = tree.cursor();
+
+		cursor.iterate((node) => {
+			const body = text.slice(node.from, node.to);
+			log.info(node.type.name, body);
+		});
+
+		// log.info(tree.type.name);
 		return format_document(document);
 	}
 }
