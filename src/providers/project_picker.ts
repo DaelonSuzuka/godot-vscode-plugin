84beb508d61f544af2f71e96dd749c53c58e44eb
David Kincaid <daelonsuzuka@gmail.com>
2025-08-08 13:27:47 -0400

    Add picker

--- src/providers/project_picker.ts
+++ src/providers/project_picker.ts
@@ -1,19 +1,28 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { ctx } from "../extension";
+import { register_command } from "../utils";
 
-const log = require('../utils').createLogger('project_picker');
-
+const log = require("../utils").createLogger("project_picker");
 
 export class GDProjectPicker {
+	widget = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
 
-    statusWidget = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
-    
-    constructor() {
-        log.debug("ProjectPicker constructor");
+	constructor() {
+		log.debug("ProjectPicker constructor");
 
-        // this.statusWidget.command = "godotTools.checkStatus";
-        this.statusWidget.show();
-        this.statusWidget.text = "$(sync~spin) Status";
+		// this.statusWidget.command = "godotTools.checkStatus";
+		this.widget.show();
+		this.widget.text = "$(sync~spin) Status";
 
-        
-    }
+		this.widget.command = "godotTools.pickProject";
+
+		ctx.subscriptions.push(
+			this.widget, //
+			register_command("pickProject", this.pickProject.bind(this)),
+		);
+	}
+
+	pickProject() {
+		vscode.window.showQuickPick(["a", "b", "c"]);
+	}
 }
