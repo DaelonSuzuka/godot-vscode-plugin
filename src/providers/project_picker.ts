12ce71269e0951e525ed26af468c91ae4b8be1d2
David Kincaid <daelonsuzuka@gmail.com>
2026-03-10 10:02:26 -0400

    stash (no context)

--- src/providers/project_picker.ts
+++ src/providers/project_picker.ts
@@ -7,12 +7,14 @@ const log = require("../utils").createLogger("project_picker");
 export class GDProjectPicker {
 	widget = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
 
+	selectedProject: string | undefined;
+
 	constructor() {
-		log.debug("ProjectPicker constructor");
+		log.info("init ProjectPicker");
 
 		// this.statusWidget.command = "godotTools.checkStatus";
 		this.widget.show();
-		this.widget.text = "$(sync~spin) Status";
+		this.widget.text = "GD Project";
 
 		this.widget.command = "godotTools.pickProject";
 
@@ -20,9 +22,25 @@ export class GDProjectPicker {
 			this.widget, //
 			register_command("pickProject", this.pickProject.bind(this)),
 		);
+
+		vscode.workspace.findFiles("**/project.godot").then((files) => {
+			log.debug(
+				`found ${files.length} project files:`,
+				files.map((f) => f.fsPath),
+			);
+			if (files.length === 1) {
+				log.debug("only 1 project detected, automatically selecting:", files[0].fsPath);
+				this.selectedProject = files[0].fsPath;
+			}
+		});
 	}
 
-	pickProject() {
-		vscode.window.showQuickPick(["a", "b", "c"]);
+	async pickProject() {
+		const projectFiles = await vscode.workspace.findFiles("**/project.godot");
+		const fileNames = projectFiles.map((f) => f.fsPath);
+
+		const selection = vscode.window.showQuickPick(fileNames);
+
+		log.debug("selection:", selection);
 	}
 }
